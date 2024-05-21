2846f2781279206e65507a8f0f0890794d333ec5
Gene Biondo <gene@unforgettable.com>
2024-05-21 15:40:17 -0700

    Update say.bat to allow multiple words
    This allows multiple parameters as words of a sentence, rather than a single word

--- hybrids/mshta/say.bat
+++ hybrids/mshta/say.bat
@@ -1,6 +1,6 @@
 :speak
 @echo off 
 setlocal enableDelayedExpansion
-set "toSay=%~1"
+set "toSay=%*"
 mshta "javascript:code(close((v=new ActiveXObject('SAPI.SpVoice')).GetVoices()&&v.Speak('!toSay!')))"
 endLocal
